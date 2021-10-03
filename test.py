07e06b87e27a6554aaa232b0d9a9dd9d2442c42c
Leopold Yang <yzs19990810@gmail.com>
2021-10-03 22:33:09 +0800

    try coco dataset 2.

--- test.py
+++ test.py
@@ -1,21 +1,52 @@
-import tensorflow as tf
-import tensorflow_datasets as tfds
+import os
+import re
+import skimage.io as io
 import matplotlib.pyplot as plt
 import numpy as np
 from pycocotools.coco import COCO
+import cv2
 
-# coco_data = tfds.load("coco")
-# coco_train, coco_test = coco_data["train"], coco_data["test"]
-# print(len(coco_train))
-# a = tf.data.Dataset.from_tensor_slices(list(coco_train))
-# for coco_example in coco_train.take(100):  # 只取一个样本
-#     image = coco_example["image"]
-#     print(image.numpy()[:, :, 0].astype(np.float32).shape)
-coco = COCO('/media/user/volume2/students/s121md215_02/Data/coco/annotations/instances_train2017.json')
-# display COCO categories and supercategories
+img_path = './Data/coco/images/train2017'
+dirFiles = os.listdir(img_path)
+I = io.imread(os.path.sep.join([img_path, dirFiles[0]]))
+name = dirFiles[0]
+num = re.sub(r'\D', "", name)
+id = int(num.lstrip('0'))
+# ret = re.match("[0-9]*", name)
+print(id)
+
+# plt.imshow(I)
+# plt.show()
+dataDir = './Data/coco/annotations'
+dataType = 'train2017'
+annFile = '{}/annotations/instances_{}.json'.format(dataDir, dataType)
+coco = COCO(annFile)
+# # display COCO categories and supercategories
 cats = coco.loadCats(coco.getCatIds())
-nms=[cat['name'] for cat in cats]
+nms = [cat['name'] for cat in cats]
 print('COCO categories: \n{}\n'.format(' '.join(nms)))
 
 nms = set([cat['supercategory'] for cat in cats])
 print('COCO supercategories: \n{}'.format(' '.join(nms)))
+
+# get all images containing given categories, select one at random
+catIds = coco.getCatIds(catNms=['person', 'dog', 'skateboard'])
+imgIds = coco.getImgIds()
+img = coco.loadImgs(imgIds[0])[0]
+# I = io.imread(img['coco_url'])
+imgIds = coco.getImgIds(imgIds=[id])
+# img = coco.loadImgs(imgIds[np.random.randint(0, len(imgIds))])[0]
+# # load and display image
+# # I = io.imread('%s/images/%s/%s'%(dataDir,dataType,img['file_name']))
+# # use url to load image
+
+# plt.axis('off')
+# plt.imshow(I)
+# plt.show()
+#
+# # load and display instance annotations
+# plt.imshow(I)
+# plt.axis('off')
+print(img['id'])
+annIds = coco.getAnnIds(imgIds=id, iscrowd=None)
+anns = coco.loadAnns(annIds)
